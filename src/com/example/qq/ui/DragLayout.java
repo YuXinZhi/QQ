dd333ec55870911c186b85c6bc019d27544e08d8
Ben <whoyumengmeng@gmail.com>
2016-04-01 23:13:27 +0800

    3

---
+++ src/com/example/qq/ui/DragLayout.java
@@ -0,0 +1,29 @@
+package com.example.qq.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * 侧滑面板
+ * 
+ * @author Ben
+ *
+ */
+public class DragLayout extends FrameLayout {
+
+	public DragLayout(Context context) {
+		this(context, null);
+	}
+
+	public DragLayout(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public DragLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+
+		// 初始化操作
+	}
+
+}
