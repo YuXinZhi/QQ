d6b8c6e7d18d6f2770691e83901e7a765fd07da5
Ben <whoyumengmeng@gmail.com>
2016-04-02 22:30:53 +0800

    4.2。22:30

--- src/com/example/qq/MainActivity.java
+++ src/com/example/qq/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.qq;
 
+import com.example.qq.ui.DragLayout;
+import com.example.qq.ui.DragLayout.OnDragStatusChangeListener;
+import com.example.qq.utils.ToastUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 
 public class MainActivity extends Activity {
@@ -13,24 +15,25 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		DragLayout mDragLayout = (DragLayout) findViewById(R.id.dl);
+		mDragLayout.setOnDragStatusChangeListener(new OnDragStatusChangeListener() {
+
+			@Override
+			public void onOpen() {
+				ToastUtils.showToastShort(MainActivity.this, "onOpen");
+			}
+
+			@Override
+			public void onDragging(float percent) {
+				ToastUtils.showToastShort(MainActivity.this, "onDragging");
+			}
+
+			@Override
+			public void onClose() {
+				ToastUtils.showToastShort(MainActivity.this, "onClose");
+			}
+		});
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
